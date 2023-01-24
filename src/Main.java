5aceba778f69eb4c9cd2affdb4666bf1171dc1a0
Nojus <nojus.stasiunas2@gmail.com>
2023-01-24 12:55:08 +0200

    add/findMostFrequent-finalUpdate

--- src/Main.java
+++ src/Main.java
@@ -10,52 +10,45 @@ public class Main {
         System.out.println(n);
         var result = selectEvenOccurrences(n);
         System.out.println(result);
-        int[] input = {1, 3, 4, 5, 3, 6, 3};
+        int[] input = {1, 2, 3, 66, 66, 51};
         System.out.println(findMostFrequent(input));
     }
 
-    //    pradinis masyvas, su skaičiais, turi surasti, kuris iš tų skaičių dažniausiai kartojasi
+//    pradinis masyvas, su skaičiais, turi surasti, kuris iš tų skaičių dažniausiai kartojasi
 //    ir grąžinti to skaičiaus indeksą pradiniame masyve
 //    pvz int[] input = {1, 3, 4, 5, 3, 6, 3}, tai turi grąžinti 6
 //    paskutinę jo vietą pradiniame masyve
-//    int[] input = {1, 2, 3} -1 turėtų lygtais grąžinti
+//    int[] input = {1, 2, 3} -1 turėtų grąžinti
+
     public static int findMostFrequent(int[] input) {
-        Map<Integer, Integer> hash = new HashMap<Integer, Integer>();
-//        int max = -1;
-//        int index = -1;
-//        for (int i = 0; i < input.length; i++){
-//            if (hash.containsKey(input[i])){
-//                int value = hash.get(i);
-//                value++;
-//                hash.get(value);
-//                if (hash.get(i) >= max){
-//                    max = hash.get(input[i]);
-//                    index = i;
-//                }
-//            }
-//            hash.get(input[i]);
-//        }
-//        if (max == 1 && max == -1){
-//            return -1;
-//        }
-//        return index;
-        for (int i : input) {
-            Integer freq = hash.get(i);
-            hash.put(i, (freq == null) ? 1 : freq + 1);
-        }
+        int max = Integer.MIN_VALUE;
+        int maxIndex = -1;
+        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> indexMap = new HashMap<>();
 
-        int max = -1;
-        int index = 0;
-        int mostFrequent = -1;
-
-        for (Map.Entry<Integer, Integer> e : hash.entrySet()) {
-            if (e.getValue() > max) {
-                mostFrequent = e.getKey();
-                max = e.getValue();
+        for (int i = 0; i < input.length; i++) {
+            int key = input[i];
+            if (map.containsKey(key)) {
+                int count = map.get(key);
+                map.put(key, ++count);
+            } else {
+                map.put(key, 1);
+                indexMap.put(key, i);
             }
         }
-        return mostFrequent;
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int key = entry.getKey();
+            int value = entry.getValue();
+            if (value > max) {
+                max = value;
+                maxIndex = indexMap.get(key);
+            }
+        }
+
+        return maxIndex;
     }
+
 
     public static ArrayList<Integer> selectEvenOccurrences(ArrayList<Integer> numbers) {
         ArrayList<Integer> evenOcc = new ArrayList<>();
