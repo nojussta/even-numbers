87f20a15aeedb0a2ef7658d43843e41cc2f90ac2
Nojus <nojus.stasiunas2@gmail.com>
2023-01-24 12:28:18 +0200

    add/findMostFrequent

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,6 +10,34 @@ public class Main {
         System.out.println(n);
         var result = selectEvenOccurrences(n);
         System.out.println(result);
+        int[] input = {1, 3, 4, 5, 3, 6, 3};
+        System.out.println(findMostFrequent(input));
+    }
+
+    //    pradinis masyvas, su skaičiais, turi surasti, kuris iš tų skaičių dažniausiai kartojasi
+//    ir grąžinti to skaičiaus indeksą pradiniame masyve
+//    pvz int[] input = {1, 3, 4, 5, 3, 6, 3}, tai turi grąžinti 6
+//    paskutinę jo vietą pradiniame masyve
+//    int[] input = {1, 2, 3} -1 turėtų lygtais grąžinti
+    public static int findMostFrequent(int[] input) {
+        Map<Integer, Integer> m = new HashMap<Integer, Integer>();
+
+        for (int i : input) {
+            Integer freq = m.get(i);
+            m.put(i, (freq == null) ? 1 : freq + 1);
+        }
+
+        int max = -1;
+        int mostFrequent = -1;
+
+        for (Map.Entry<Integer, Integer> e : m.entrySet()) {
+            if (e.getValue() > max) {
+                mostFrequent = e.getKey();
+                max = e.getValue();
+            }
+        }
+
+        return mostFrequent;
     }
 
     public static ArrayList<Integer> selectEvenOccurrences(ArrayList<Integer> numbers) {
